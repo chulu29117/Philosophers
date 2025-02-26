449454dc3dcb984ff37ca96e9f22d39d263672ed
chuchulu29117 <chuchulu117@gmail.com>
2025-02-26 15:37:00 +0200

    files added

---
+++ philo/include/philo.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clu <clu@student.hive.fi>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/26 15:32:05 by clu               #+#    #+#             */
+/*   Updated: 2025/02/26 15:35:01 by clu              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <sys/time.h>
+
+
+
+#endif
